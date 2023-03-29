859851810db1f3c04d05eadfee0b94e4fe9d5e49
vsiver <v.siverskiy@gmail.com>
2023-03-29 22:35:34 +0300

    first commit

---
+++ src/test/java/com/vsiver/spring/car_rent_project/CarRentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsiver.spring.car_rent_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
